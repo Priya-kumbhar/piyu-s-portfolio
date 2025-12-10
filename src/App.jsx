0545f71656e1f64c480220313a8d1d9f5353ed82
Priyanka Kumbhar <priyankakumbhar.2405@gmail.com>
2025-12-10 23:56:37 +0530

    feat: completed about section with 3D element, text, visuals and framer-motion animation

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ function App() {
                    {/* testimonial */}
                       {/* contact */}
                          {/* footer */}
-
     </div>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeroText from '../components/HeroText';
 import ParallaxBackground from '../components/ParallaxBackground';
 
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Astronaut } from '../components/Astronaut';
+import { useMediaQuery } from 'react-responsive';
+
 function Hero() {
+  const isMobile=useMediaQuery ({maxWidth :853})
   return <section className='flex items-start
   justify-center md:items-start md:justify-start
   min-h-screen overflow-hidden c-space'>
@@ -11,7 +17,12 @@ function Hero() {
     <figure className='absolute inset-0' 
     style={{
         width:"100vw",height:"100vh"
-    }}></figure>
+    }}>
+      <Canvas camera={{position:[0,1,3]}}>
+    <Astronaut scale={0.23} position={[0,-1.5,0]}/>
+    <OrbitControls/>
+      </Canvas>
+    </figure>
 </section>
 }
 
