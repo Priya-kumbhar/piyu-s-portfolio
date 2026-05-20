76dcd96c7decdd37a11d44ffed4b7706406808cd
Priyanka Kumbhar <priyankakumbhar.2405@gmail.com>
2026-05-20 16:27:36 +0530

    updated about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -34,19 +34,32 @@ const About =()=> {
      <div className="flex  grid-default-color grid-2">
       <div ref={grid2Container} className="flex items-center justify-center w-full h-full">
         <p className="flex items-end text-5xl text-gray-500">
-        CODE IS CRAFT
+          Skills
         </p>
-        <Card style={{ rotate:"75deg" ,top:"30%", left:"20%"}}text="GRASP" containerRef={grid2Container}/>
-          <Card  style={{ rotate:"-30deg" ,top:"60%", left:"45%"}}text="SOLID" containerRef={grid2Container}  />
-            <Card  style={{ rotate:"90deg" ,top:"30%", left:"70%"}}text="Design Pattern" containerRef={grid2Container}/>
-              <Card  style={{ rotate:"-45deg" ,top:"55%", left:"0%"}}text="Design Principles" containerRef={grid2Container}/>
-                <Card  style={{ rotate:"20deg" ,top:"10%", left:"38%"}} text="SRP" containerRef={grid2Container}/>
+        <Card style={{ rotate:"75deg" ,top:"30%", left:"20%"}}
+        text="Next.js" containerRef={grid2Container}/>
+          <Card  style={{ rotate:"-30deg" ,top:"60%", left:"45%"}}
+          text="PostgreSQL" containerRef={grid2Container}  />
+            <Card  style={{ rotate:"90deg" ,top:"30%", left:"70%"}}
+            text="React" containerRef={grid2Container}/>
+              <Card  style={{ rotate:"-45deg" ,top:"55%", left:"0%"}}
+              text="MongoDB" containerRef={grid2Container}/>
+                <Card  style={{ rotate:"20deg" ,top:"10%", left:"38%"}}
+                 text="Node.js" containerRef={grid2Container}/>
+                  <Card  style={{ rotate:"20deg" ,top:"50%", left:"48%"}}
+                 text="JavaScript" containerRef={grid2Container}/>
+                  <Card  style={{ rotate:"20deg" ,top:"40%", left:"68%"}}
+                 text="TypeScript" containerRef={grid2Container}/>
+                  <Card  style={{ rotate:"20deg" ,top:"90%", left:"54%"}}
+                 text="Gen AI" containerRef={grid2Container}/>
+
+
+
                  <Card  style={{ rotate:"30deg" ,top:"70%", left:"70%"}} 
-                 image="assets/logos/csharp-pink.png"/>
-         <Card  style={{ rotate:"-45deg" ,top:"70%", left:"25%"}}
-          image="assets/logos/dotnet-pink.png"/>
+                 image=""/><Card  style={{ rotate:"-45deg" ,top:"70%", left:"25%"}}
+                 image=""/>
                 <Card  style={{ rotate:"-45deg" ,top:"5%", left:"10%"}}
-          image="assets/logos/blazor-pink.png"/>
+                image=""/>
                 
         </div></div>  
      <div className="flex  grid-black-color grid-3">
